86e3796a0eb0595aa6d85e40647a572f2092d425
gladi <gladi@LAPTOP-IFRBPTMB>
2024-06-20 23:48:12 +0530

    commit3

---
+++ mymavenprojcet3/src/main/java/mavenpack3/CoditionLoop.java
@@ -0,0 +1,19 @@
+package mavenpack3;
+
+public class CoditionLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=6;
+		if(a%2==0)
+		{
+		System.out.println("even number");	
+		}
+		else
+		{
+			System.out.println("odd number");
+		}
+
+	}
+
+}
